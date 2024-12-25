4cb306c738a147a233a38834c39c5ea50ddc5917
tusherbhakta <tusheranta@gmail.com>
2024-12-26 04:15:16 +0530

    added wishlist to the local storage

---
+++ src/utility/wishlist.js
@@ -0,0 +1,26 @@
+import { stringify } from "postcss";
+
+const getStoredWishlist = () =>{
+    const storedListStr = localStorage.getItem('wishlist');
+    if(storedListStr){
+        const storeedList = JSON.parse(storedListStr);
+        return storeedList;
+    }
+    else{
+        return[];
+    }
+
+}
+
+const addToWishlist = (id) =>{
+    const storedList = getStoredWishlist();
+    if(storedList.includes(id)){
+        console.log('this is already exist!');
+    }
+    else{
+        storedList.push(id);
+        const storedListStr = JSON.stringify(storedList);
+        localStorage.setItem('wishlist',storedListStr);
+    }
+}
+export{addToWishlist};
